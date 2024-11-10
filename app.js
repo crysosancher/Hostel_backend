1423b95b3868259b5a311220d226e378d2622ade
crysosancher <vibhupandey.vp@gmail.com>
2024-11-10 19:17:37 +0530

    cors

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const config = require('./config/env');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');  // Add cors import
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');  // Add more routes as needed
 
 const app = express();
+
+// Enable CORS
+app.use(cors());  // This allows all origins by default
+
 app.use(bodyParser.json());
 
 // Routes
