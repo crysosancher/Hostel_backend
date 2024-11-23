ab820b729cb7d58b34f92d5c4dc61ea661b3aed3
crysosancher <vibhupandey.vp@gmail.com>
2024-11-24 04:27:31 +0530

    added 401

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,7 +6,7 @@ function authMiddleware(req, res, next) {
     if (!token) return res.status(403).json({ message: 'No token provided.' });
 
     jwt.verify(token, config.jwt.secret, (err, decoded) => {
-        if (err) return res.status(500).json({ message: 'Failed to authenticate token.' });
+        if (err) return res.status(401).json({ message: 'Failed to authenticate token.' });
         req.user = decoded;
         next();
     });
