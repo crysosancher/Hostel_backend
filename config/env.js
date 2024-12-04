2624d81b7c656f6be4706c001a7a6da3ef669607
crysosancher <vibhupandey.vp@gmail.com>
2024-12-04 14:17:25 +0530

    dev url

--- config/env.js
+++ config/env.js
@@ -4,7 +4,9 @@ const config = {
     db: {
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        connectString: process.env.DB_CONNECTION_STRING,
+        connectString: process.env.DB_CONNECTION_STRING
+        // testUser: process.env.DB_TEST_USER,
+        // testPassword: process.env.DB_TEST_PASSWORD,
     },
     jwt: {
         secret: process.env.JWT_SECRET,
